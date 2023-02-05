427ba117607c2b8cc350b2b5c00a8957e3fece5d
Bruno Gomes <74106187+brunaogomes11@users.noreply.github.com>
2023-02-05 00:27:11 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -43,7 +43,7 @@ def image():
     if request.method == 'POST':
         if 'file' not in request.files:
             return redirect(request.url)
-        imagem = request.files.get('file')
+        imagem = request.files.get('photo')
         if not imagem:
             return
         resultado = classificar_imagem(imagem)
