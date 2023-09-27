f6c4478083370c16b16a311de34e288da99e4bfe
Bruno Gomes <74106187+brunaogomes11@users.noreply.github.com>
2023-09-27 17:39:09 -0300

    Add files via upload

--- app/covid.py
+++ app/covid.py
@@ -2,51 +2,51 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 import numpy as np
+
 torch.manual_seed(123)
 # Contrução do Modelo
 class classificador(nn.Module):
-    def __init__(self):
-        super().__init__()
+  def __init__(self):
+      super().__init__()
 
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3,3)) 
-        self.conv2 = nn.Conv2d(64, 64, (3,3))
-        self.activation = nn.ReLU()
-        self.bnorm = nn.BatchNorm2d(num_features=64)
-        self.pool = nn.MaxPool2d(kernel_size=(2,2))
-        self.flatten = nn.Flatten()
+      self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3,3)) 
+      self.conv2 = nn.Conv2d(64, 64, (3,3))
+      self.activation = nn.ReLU()
+      self.bnorm = nn.BatchNorm2d(num_features=64)
+      self.pool = nn.MaxPool2d(kernel_size=(2,2))
+      self.flatten = nn.Flatten()
 
-        # output = (input - filter + 1) / stride
-        # Convolução 1 -> (64 - 3 + 1) / 1 = 62x62
-        # Pooling 1 -> Só dividir pelo kernel_size = 31x31
-        # Convolução 2 -> (31 - 3 + 1)/ 1 = 29x29
-        # Pooling 2 -> Só dividir pelo kernel_size = 14x14
-        # 14 * 14 * 64
-        # 33907200 valores -> 256 neurônios da camada oculta
-        self.linear1 = nn.Linear(in_features=14*14*64, out_features=256)
-        self.linear2 = nn.Linear(256, 128)
-        self.output = nn.Linear(128, 3)
+      # output = (input - filter + 1) / stride
+      # Convolução 1 -> (64 - 3 + 1) / 1 = 62x62
+      # Pooling 1 -> Só dividir pelo kernel_size = 31x31
+      # Convolução 2 -> (31 - 3 + 1)/ 1 = 29x29
+      # Pooling 2 -> Só dividir pelo kernel_size = 14x14
+      # 14 * 14 * 64
+      # 33907200 valores -> 256 neurônios da camada oculta
+      self.linear1 = nn.Linear(in_features=14*14*64, out_features=256)
+      self.linear2 = nn.Linear(256, 128)
+      self.output = nn.Linear(128, 3)
 
-    def forward(self, X):
-        X = self.pool(self.bnorm(self.activation(self.conv1(X))))
-        X = self.pool(self.bnorm(self.activation(self.conv2(X))))
-        X = self.flatten(X)
+  def forward(self, X):
+      X = self.pool(self.bnorm(self.activation(self.conv1(X))))
+      X = self.pool(self.bnorm(self.activation(self.conv2(X))))
+      X = self.flatten(X)
 
-        # Camadas densas
-        X = self.activation(self.linear1(X))
-        X = self.activation(self.linear2(X))
-        
-        # Saída
-        X = self.output(X)
+      # Camadas densas
+      X = self.activation(self.linear1(X))
+      X = self.activation(self.linear2(X))
+      
+      # Saída
+      X = self.output(X)
 
-        return X
-
+      return X
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 device
+
 classificadorLoaded = classificador()
 state_dict = torch.load('app/checkpoint.pth')
 classificadorLoaded.load_state_dict(state_dict)
-
 def transform_imagem(imagem_teste):
     from PIL import Image
     imagem = Image.open(imagem_teste)
@@ -68,4 +68,3 @@ def classificar_imagem(file):
     output = output.detach().numpy()
     resultado = np.argmax(output)
     return resultado
-
--- app/main.py
+++ app/main.py
@@ -1,10 +1,8 @@
 # Start Server
-from app.covid import classificar_imagem
+from app.covid import classificar_imagem, transform_imagem
 import os
 from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
-from threading import Thread
-from app.controls import delete_files
 
 app = Flask(__name__)
 
@@ -14,16 +12,16 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    thread = Thread(target=delete_files)
-    thread.daemon = True
-    thread.start()
+    dir = os.listdir(app.config['UPLOAD_FOLDER'])
+    for i in range(0, len(dir)):
+        os.remove(app.config['UPLOAD_FOLDER']+dir[i])
     if request.method == 'POST':
         if 'file' not in request.files:
             return redirect(request.url)
         imagem = request.files.get('file')
         if not imagem:
             return
-        filename = secure_filename(imagem.filename)
+        filename = secure_filename("uploads/"+imagem.filename)
         imagem.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         resultado = classificar_imagem(imagem)
         if resultado == 0:
@@ -37,23 +35,20 @@ def upload_file():
 
 @app.route('/display/<filename>')
 def display_image(filename):
-	return redirect(url_for('static', filename='/uploads/' + filename), code=301)
+    return redirect(url_for('static', filename='/uploads/' + filename), code=301)
 
-@app.route("/uploadedRN", methods=['GET','POST'])
+@app.route("/uploadImage", methods=['GET','POST'])
 def imageUpload():
-    imagem = request.files['image']
-    resultado = classificar_imagem(imagem)
-    if resultado == 0:
-        resultado = 'Resultado: Covid'
-    elif resultado == 1:
-        resultado = 'Resultado: PNEUMONIA não detectada e COVID não detectado'
-    elif resultado == 2:
-        resultado = 'Resultado: Pneumonia'
+    uploaded_file = request.files.get('uploaded_file')
+    if uploaded_file:
+        resultado = classificar_imagem(uploaded_file)
+        if resultado == 0:
+            resultado = 'Resultado: Covid'
+        elif resultado == 1:
+            resultado = 'Resultado: Pneumonia não detectada e Covid não detectado'
+        elif resultado == 2:
+            resultado = 'Resultado: Pneumonia'
     return {"message": resultado}
-
-@app.route('/about')
-def about_page():
-    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
